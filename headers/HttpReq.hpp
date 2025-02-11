adc7f90e5725cd9a4d7304a934d699d42c84fa3d
bschneid <bschneid@student.42berlin.de>
2025-02-11 15:05:36 +0100

    magic cleaning

--- headers/HttpReq.hpp
+++ headers/HttpReq.hpp
@@ -5,8 +5,9 @@
 
 class HttpReq {
 private:
-	time_t	_creationTime;
-	Server	*_server;
+	// - Static variables:
+	time_t		_creationTime;
+	Server		*_server;
 
 	std::string	_buffer;
 	int			_httpStatus;
@@ -16,24 +17,19 @@ private:
 	std::string	_protocol;
 
 	std::map<std::string, std::string> _headers;	// Headers are case-insensitive (tolower-ed)
-    
+	
 	std::string _body;
 
-	bool	_startlineParsed;	// Whether the start line is fully received
-	bool	_headersParsed;		// Whether the headers are fully received
-	bool	_isChunked;     	// Whether the body uses chunked transfer
-	bool	_bodyComplete;  	// Whether the body is fully received
-
-    size_t	_currentChunkSize;	// Size of the current chunk being read
+	bool		_startlineParsed;	// Whether the start line is fully received
+	bool		_headersParsed;		// Whether the headers are fully received
+	bool		_isChunked;     	// Whether the body uses chunked transfer
+	bool		_bodyComplete;  	// Whether the body is fully received
+	size_t		_currentChunkSize;	// Size of the current chunk being read
 	
 	// Helper function to trim leading and trailing whitespaces
 	bool	verifyHeaders(void);
 	bool	headersAreParsed(void) const;
 
-
-	// - Request headers:	*additional context to a request or add extra logic* -> case-insensitive string followed by a colon (:) and a value.
-	// - Representation headers:	*if message has a body*
-	// - Request body:
 	bool	parseStartLine(void);
 	bool	isValidMethod(void) const;
 	bool	isValidTarget(void) const;
@@ -50,24 +46,21 @@ public:
 	HttpReq operator=(const HttpReq &another);
 	~HttpReq();
 
-	// --> Set-methods:
-
 	// --> Get-methods:
 	const std::string	&getMethod(void) const;
 	const std::string	&getTarget(void) const;
 	const std::string	&getProtocol(void) const;
 	const std::string	&getHeader(const std::string &key) const;
 	const std::map<std::string, std::string>	&getHeaders(void) const;
-	const std::string	&getBody(void);
-	int					getHttpStatus(void) const;
-
+	
+	// --> Processing methods:
 	bool	processData(Server &server, const std::string &data);	// Encapsulates the parsing of the incoming data
+	const std::string	&getBody(void);								// Get the body of the request
+	int					getHttpStatus(void) const;					// Get the status of the request
 
-	// print content
-	void	print(void) const;
+	void	print(void) const;										// print content
 
-	// Reset for a new request
-    void	reset(void);
+	void	reset(void);											// Reset for a new request
 };
 
 #endif
